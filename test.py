b27d31a9ab559633bb64b2f49339db6974587de9
wisdom_teeth <zhhhp520@gmail.com>
2017-03-15 18:56:59 +0800

    test 文件 genertor 中的 send

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 def consumer():
     r = ''
-    while True:
-        n = yield r
+    while True: #step3 无限执行
+        n = yield r #step4 执行到最后 r = '200 OK' 再回到这一行，将 r 返回给 produce 的 'r = c.send(n)' 中的 r 并继续执行 produce 中的代码
         if not n:
             return
         print('[CONSUMER] Consuming %s...' % n)
         r = '200 OK'
 
 def produce(c):
-    c.send(None)
+    c.send(None) #step1 执行 consumer 到 yield r
     n = 0
     while n < 5:
         n = n + 1
         print('[PRODUCER] Producing %s...' % n)
-        r = c.send(n)
+        r = c.send(n) #step2 将 n == 1 传给 'n = yield r' 中的 n 并继续执行 consumer 中代码
         print('[PRODUCER] Consumer return: %s' % r)
     c.close()
 
