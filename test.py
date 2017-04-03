e5da3fd93e68c31dc044a63e5e55f943704c1794
wisdom_teeth <zhhhp520@gmail.com>
2017-04-03 17:00:17 +0800

    test.py selenmiu

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-import requests ##导入requests
-from bs4 import BeautifulSoup ##导入bs4中的BeautifulSoup
-import os
+#coding=utf-8
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+driver = webdriver.Chrome()
+driver.get("http://www.baidu.com")
+
+driver.find_element_by_xpath("//form[@id='form' and @class='fm']/span/input[@id='kw']").send_keys('nh')
+
+driver.find_element_by_xpath("//form[@id='form']/span[2]/input")
+
+
+#driver.find_element_by_link_text(u'新闻').click()
+#driver.find_element_by_xpath("//input[@id='kw']").send_keys('hh')
+# inputs = driver.find_elements_by_tag_name('input')
+# for i in inputs:
+#     if i.get_attribute('autocomplete') == 'off':
+#         i.send_keys('zh')
+
+print(driver.title)
